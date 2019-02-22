6adde274ae1741d068e0c7f961a3c9cb84b4d3e2
HanSwinkels <hanswinkels@hotmail.com>
2019-02-22 12:09:22 +0100

    Fixed typo

--- postLoadingQueries.sql
+++ postLoadingQueries.sql
@@ -1,3 +1,3 @@
-SELECT * FROM CourseOffers, Courses, Degrees, Teachers WHERE CourseOffers.courseOfferId = 1;
+SELECT * FROM CourseOffers, Courses, Degrees, Teachers WHERE CourseOffers.CourseOfferId = 1;
 SELECT StudentAssistants.CourseOfferId, CourseId, Year, Quartile, StudentId, StudentName, Address, BirthYearStudent, Gender, DegreeId, Dept, DegreeDescription, TotalECTS FROM StudentAssistants, CourseOffers, Students, Degrees WHERE StudentAssistents.StudenRegistrationId=140;
 SELECT AVG(Grade) FROM CourseRegistrations WHERE CourseRegistrations.StudentRegistrationId=140;
