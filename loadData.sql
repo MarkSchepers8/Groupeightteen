7ec5b9e5ec1fe9984bb19dd76b89f8ee4ac51e09
HanSwinkels <hanswinkels@hotmail.com>
2019-02-28 00:54:55 +0100

    Changed copy from to correct folder for server

--- loadData.sql
+++ loadData.sql
@@ -1,27 +1,27 @@
 COPY students(studentid, studentname, address, birthyearstudent, gender)
-FROM '/home/student/tables/Students.table'
+FROM '/mnt/ramdisk/tables/Students.table'
 DELIMITER ',' CSV HEADER;
 COPY degrees(degreeid, dept, degreedescription, totalects)
-FROM '/home/student/tables/Degrees.table'
+FROM '/mnt/ramdisk/tables/Degrees.table'
 DELIMITER ',' CSV HEADER;
 COPY studentregistrationstodegrees(studentregistrationid, studentid, degreeid, registrationyear)
-FROM '/home/student/tables/StudentRegistrationsToDegrees.table'
+FROM '/mnt/ramdisk/tables/StudentRegistrationsToDegrees.table'
 DELIMITER ',' CSV HEADER;
 COPY teachers(teacherid, teachername, address, birthyearteacher, gender)
-FROM '/home/student/tables/Teachers.table'
+FROM '/mnt/ramdisk/tables/Teachers.table'
 DELIMITER ',' CSV HEADER;
 COPY courses(courseid, coursename, coursedescription, degreeid, ects)
-FROM '/home/student/tables/Courses.table'
+FROM '/mnt/ramdisk/tables/Courses.table'
 DELIMITER ',' CSV HEADER;
 COPY courseoffers(courseofferid, courseid, year, quartile)
-FROM '/home/student/tables/CourseOffers.table'
+FROM '/mnt/ramdisk/tables/CourseOffers.table'
 DELIMITER ',' CSV HEADER;
 COPY teacherassignmentstocourses(courseofferid, teacherid)
-FROM '/home/student/tables/TeacherAssignmentsToCourses.table'
+FROM '/mnt/ramdisk/tables/TeacherAssignmentsToCourses.table'
 DELIMITER ',' CSV HEADER;
 COPY studentassistants(courseofferid, studentregistrationid)
-FROM '/home/student/tables/StudentAssistants.table'
+FROM '/mnt/ramdisk/tables/StudentAssistants.table'
 DELIMITER ',' CSV HEADER;
 COPY courseregistrations(courseofferid, studentregistrationid, grade)
-FROM '/home/student/tables/CourseRegistrations.table'
+FROM '/mnt/ramdisk/tables/CourseRegistrations.table'
 DELIMITER ',' CSV HEADER NULL AS 'null';" 
