0217c99d170f12ce730ab5af63a4039bdff6ceaa
HanSwinkels <hanswinkels@hotmail.com>
2019-02-28 00:52:00 +0100

    Updated queries 6,7,8 now working

--- q2Create.sql
+++ q2Create.sql
@@ -1,11 +1,10 @@
 CREATE MATERIALIZED VIEW GPA as (
-    SELECT StudentRegistrationId, CAST(sum(grade*ects) as float)/CAST(sum(ects) as float) as gpa
+    SELECT StudentRegistrationsToDegrees.StudentRegistrationId, CAST(sum(grade*ects) as float)/CAST(sum(ects) as float) as gpa
     FROM CourseRegistrations
-    WHERE CourseRegistrations.Grade >= 5
-    LEFT JOIN StudentRegistrationsToDegrees ON StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId
+    LEFT JOIN StudentRegistrationsToDegrees ON StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId AND CourseRegistrations.Grade >= 5 
     LEFT JOIN CourseOffers ON CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId
     LEFT JOIN Courses on Courses.CourseId = CourseOffers.CourseId
-    GROUP BY StudentRegistrationId
+    GROUP BY StudentRegistrationsToDegrees.StudentRegistrationId
 );
 
 CREATE MATERIALIZED VIEW HighestGradeCourseOffers as (
@@ -15,29 +14,46 @@ CREATE MATERIALIZED VIEW HighestGradeCourseOffers as (
         FROM CourseRegistrations
         GROUP By CourseOfferId
     ) AS maxCr
-    INNER JOIN CourseRegistrations AS cr ON maxCr.CourseOfferId = cr.CourseOfferId AND maxCr.grade = cr.HighestGrade
+    INNER JOIN CourseRegistrations AS cr ON maxCr.CourseOfferId = cr.CourseOfferId AND maxCr.highestgrade = cr.grade
     LEFT JOIN StudentRegistrationsToDegrees ON StudentRegistrationsToDegrees.StudentRegistrationId = cr.StudentRegistrationId 
+    LEFT JOIN CourseOffers ON CourseOffers.CourseOfferId = cr.CourseOfferId
 );
 
 CREATE MATERIALIZED VIEW StudentsECTS as (
-    SELECT StudentId, StudentRegistrationId, DegreeId, SUM(ects) as totalects
+    SELECT DISTINCT StudentId, StudentRegistrationsToDegrees.StudentRegistrationId, StudentRegistrationsToDegrees.DegreeId, SUM(ects) as totalects
     FROM StudentRegistrationsToDegrees
     LEFT JOIN CourseRegistrations ON StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId AND grade >=5 
     LEFT JOIN CourseOffers ON CourseOffers.CourseOfferId = CourseRegistrations.CourseOfferId
     LEFT JOIN Courses ON Courses.CourseId = CourseOffers.CourseId
-    GROUP BY StudentRegistrationId
-)
+    GROUP BY StudentRegistrationsToDegrees.StudentRegistrationId
+);
 
 CREATE MATERIALIZED VIEW ActiveStudentsPerDegree as (
-    SELECT StudentId, StudentRegistrationId, DegreeId
+    SELECT StudentId, StudentRegistrationId, Degrees.DegreeId
     FROM StudentsECTS
     LEFT JOIN Degrees ON StudentsECTS.DegreeId = Degrees.DegreeId
-    HAVING StudentsECTS.totalects < totalects
+    GROUP BY StudentRegistrationId, StudentId, Degrees.DegreeId, Degrees.TotalECTS, StudentsECTS.TotalECTS
+    HAVING StudentsECTS.totalects < degrees.totalects
 );
 
 CREATE MATERIALIZED VIEW DegreeCompleted as (
-    SELECT StudentId, StudentRegistrationId FROM StudentsECTS
+    SELECT StudentId, StudentRegistrationId 
+    FROM StudentsECTS
     LEFT JOIN Degrees ON Degrees.DegreeId = StudentsECTS.DegreeId
+    GROUP BY StudentRegistrationId, StudentId, Degrees.DegreeId, Degrees.TotalECTS, StudentsECTS.TotalECTS
     HAVING Degrees.TotalECTS <= StudentsECTS.TotalECTS
+);
+
+CREATE MATERIALIZED VIEW NumAssistantsCourseOffer as(
+   SELECT CourseOfferId, Count(StudentRegistrationId) as NumAssistants
+   FROM StudentAssistants
+   GROUP BY CourseOfferId
+   HAVING Count(StudentRegistrationId)>0
+);
+
+CREATE MATERIALIZED VIEW NumStudentsCourseOffer as(
+   SELECT CourseOfferId, Count(StudentRegistrationId) as NumStudents
+   FROM CourseRegistrations
+   GROUP BY CourseOfferId
 );
 
--- q2Queries.sql
+++ q2Queries.sql
@@ -22,25 +22,11 @@ AND CourseRegistrations.Grade >= 5
 -- QUERY 5
 
 -- QUERY 6
-SELECT DISTINCT StudentId, count(StudentId)
-FROM HighestGradeCourseOffers
-WHERE Year = 2018 AND Quartile = 1
-GROUP BY StudentId
-HAVING count(studentid) >= %1%;
+SELECT DISTINCT StudentId, count(StudentId) FROM HighestGradeCourseOffers WHERE Year = 2018 AND Quartile = 1 GROUP BY StudentId HAVING count(studentid) >= %1%;
 
 -- QUERY 7
-SELECT degreeid, birthyearstudent, gender, avg(gpa)
-FROM Students
-LEFT JOIN ActiveStudentsPerDegree ON ActiveStudentsPerDegree.StudentId = Students.StudentId
-LEFT JOIN GPA ON GPA.StudentRegistrationId = ActiveStudentsPerDegree.StudentRegistrationId
-GROUP BY cube(degreeid, birthyearstudent, gender);
+SELECT degreeid, birthyearstudent, gender, avg(gpa) FROM Students LEFT JOIN ActiveStudentsPerDegree ON ActiveStudentsPerDegree.StudentId = Students.StudentId LEFT JOIN GPA ON GPA.StudentRegistrationId = ActiveStudentsPerDegree.StudentRegistrationId GROUP BY cube(degreeid, birthyearstudent, gender);
 
 -- QUERY 8
-SELECT CourseName, Year, Quartile
-FROM CourseOffers 
-LEFT JOIN StudentAssistants ON StudentAssistants.CourseOfferId = CourseOffers.CourseOfferId
-LEFT JOIN CourseRegistrations ON CourseRegistrations.CourseId = CourseOffers.CourseId
-LEFT JOIN Courses ON CourseOffers.CourseOfferId = Courses.CourseId
-GROUP BY CourseOfferId
-HAVING COUNT(CourseRegistrations.StudentRegistrationId)/COUNT(StudentAssistants.StudentRegistrationId)>50
-ORDER BY CourseOfferId;
+SELECT CourseName, Year, Quartile FROM NumAssistantsCourseOffer LEFT JOIN NumStudentsCourseOffer ON NumAssistantsCourseOffer.CourseOfferId = NumStudentsCourseOffer.CourseOfferId AND NumStudents/NumAssistants>50 LEFT JOIN CourseOffers ON NumStudentsCourseOffer.CourseOfferId = CourseOffers.CourseOfferId
+LEFT JOIN Courses ON Courses.CourseId = CourseOffers.CourseOfferId  ORDER BY CourseOffers.CourseOfferId;
