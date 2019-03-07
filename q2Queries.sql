0ef1967c4219b271d784a93b5f3c47c7a9c8e7e5
HanSwinkels <hanswinkels@hotmail.com>
2019-03-07 19:12:06 +0100

    Bug fixes query 6

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,8 +1,9 @@
+<<<<<<< HEAD
 SELECT CourseName, Grade FROM Students LEFT JOIN StudentRegistrationsToDegrees ON Students.StudentId = StudentRegistrationsToDegrees.StudentId AND StudentRegistrationsToDegrees.DegreeId = %2% AND Students.StudentId = %1% LEFT JOIN Courses ON Courses.DegreeId = StudentRegistrationsToDegrees.DegreeId LEFT JOIN CourseOffers ON CourseOffers.CourseOfferId = Courses.CourseOfferId LEFT JOIN CourseRegistrations ON CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId WHERE Grade > 5 ORDER BY Year THEN BY Quartile THEN BY CourseOfferId;
 SELECT DISTINCT StudentId FROM DegreeCompleted LEFT JOIN CourseRegistrations ON CourseRegistrations.StudentRegistrationId = DegreeCompleted.StudentRegistrationId LEFT JOIN GPA ON GPA.StudentRegistrationId = DegreeCompleted.StudentRegistrationId AND GPA.GPA > %1% GROUP BY StudentId;
 SELECT 0;
 SELECT 0;
 SELECT 0;
-SELECT DISTINCT StudentId, count(StudentId) FROM HighestGradeCourseOffers WHERE Year = 2018 AND Quartile = 1 GROUP BY StudentId HAVING count(studentid) >= %1%;
+SELECT StudentId, count(StudentId) as numberOfCoursesWhereExcellent FROM HighestGradeCourseOffers WHERE Year = 2018 AND Quartile = 1 GROUP BY StudentId HAVING count(studentid) >= %1%;
 SELECT degreeid, birthyearstudent, gender, avg(gpa) FROM Students LEFT JOIN ActiveStudentsPerDegree ON ActiveStudentsPerDegree.StudentId = Students.StudentId LEFT JOIN GPA ON GPA.StudentRegistrationId = ActiveStudentsPerDegree.StudentRegistrationId GROUP BY cube(degreeid, birthyearstudent, gender);
 SELECT CourseName, Year, Quartile FROM NumAssistantsCourseOffer LEFT JOIN NumStudentsCourseOffer ON NumAssistantsCourseOffer.CourseOfferId = NumStudentsCourseOffer.CourseOfferId AND NumStudents/NumAssistants>50 LEFT JOIN CourseOffers ON NumStudentsCourseOffer.CourseOfferId = CourseOffers.CourseOfferId LEFT JOIN Courses ON Courses.CourseId = CourseOffers.CourseOfferId  ORDER BY CourseOffers.CourseOfferId;
