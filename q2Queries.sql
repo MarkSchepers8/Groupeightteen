852d60bda2ee43b768f58ea2e6fd8f051f2b8fc8
HanSwinkels <hanswinkels@hotmail.com>
2019-03-07 19:13:56 +0100

    Removed unnecessary top line

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 SELECT CourseName, Grade FROM Students LEFT JOIN StudentRegistrationsToDegrees ON Students.StudentId = StudentRegistrationsToDegrees.StudentId AND StudentRegistrationsToDegrees.DegreeId = %2% AND Students.StudentId = %1% LEFT JOIN Courses ON Courses.DegreeId = StudentRegistrationsToDegrees.DegreeId LEFT JOIN CourseOffers ON CourseOffers.CourseOfferId = Courses.CourseOfferId LEFT JOIN CourseRegistrations ON CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId WHERE Grade > 5 ORDER BY Year THEN BY Quartile THEN BY CourseOfferId;
 SELECT DISTINCT StudentId FROM DegreeCompleted LEFT JOIN CourseRegistrations ON CourseRegistrations.StudentRegistrationId = DegreeCompleted.StudentRegistrationId LEFT JOIN GPA ON GPA.StudentRegistrationId = DegreeCompleted.StudentRegistrationId AND GPA.GPA > %1% GROUP BY StudentId;
 SELECT 0;
