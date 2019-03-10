cfba03caf9d4c2c1e708adb01cef44fdf77fa203
HanSwinkels <hanswinkels@hotmail.com>
2019-03-10 21:43:42 +0100

    Changed everyting to inner joins

--- q2Create.sql
+++ q2Create.sql
@@ -1,9 +1,9 @@
 CREATE MATERIALIZED VIEW GPA as (
     SELECT StudentRegistrationsToDegrees.StudentRegistrationId, CAST(sum(grade*ects) as float)/CAST(sum(ects) as float) as gpa
     FROM CourseRegistrations
-    LEFT JOIN StudentRegistrationsToDegrees ON StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId AND CourseRegistrations.Grade >= 5
-    LEFT JOIN CourseOffers ON CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId
-    LEFT JOIN Courses on Courses.CourseId = CourseOffers.CourseId
+    INNER JOIN StudentRegistrationsToDegrees ON StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId AND CourseRegistrations.Grade >= 5
+    INNER JOIN CourseOffers ON CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId
+    INNER JOIN Courses on Courses.CourseId = CourseOffers.CourseId
     GROUP BY StudentRegistrationsToDegrees.StudentRegistrationId
 );
 
@@ -28,16 +28,16 @@ CREATE MATERIALIZED VIEW HighestGradeCourseOffers as (
 CREATE MATERIALIZED VIEW StudentsECTS as (
     SELECT DISTINCT StudentId, StudentRegistrationsToDegrees.StudentRegistrationId, StudentRegistrationsToDegrees.DegreeId, SUM(ects) as totalects
     FROM StudentRegistrationsToDegrees
-    LEFT JOIN CourseRegistrations ON StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId AND grade >=5 
-    LEFT JOIN CourseOffers ON CourseOffers.CourseOfferId = CourseRegistrations.CourseOfferId
-    LEFT JOIN Courses ON Courses.CourseId = CourseOffers.CourseId
+    INNER JOIN CourseRegistrations ON StudentRegistrationsToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId AND grade >=5 
+    INNER JOIN CourseOffers ON CourseOffers.CourseOfferId = CourseRegistrations.CourseOfferId
+    INNER JOIN Courses ON Courses.CourseId = CourseOffers.CourseId
     GROUP BY StudentRegistrationsToDegrees.StudentRegistrationId
 );
 
 CREATE MATERIALIZED VIEW ActiveStudentsPerDegree as (
     SELECT StudentId, StudentRegistrationId, Degrees.DegreeId
     FROM StudentsECTS
-    LEFT JOIN Degrees ON StudentsECTS.DegreeId = Degrees.DegreeId
+    INNER JOIN Degrees ON StudentsECTS.DegreeId = Degrees.DegreeId
     GROUP BY StudentRegistrationId, StudentId, Degrees.DegreeId, Degrees.TotalECTS, StudentsECTS.TotalECTS
     HAVING StudentsECTS.totalects < degrees.totalects
 );
@@ -45,7 +45,7 @@ CREATE MATERIALIZED VIEW ActiveStudentsPerDegree as (
 CREATE MATERIALIZED VIEW DegreeCompleted as (
     SELECT StudentId, StudentRegistrationId 
     FROM StudentsECTS
-    LEFT JOIN Degrees ON Degrees.DegreeId = StudentsECTS.DegreeId
+    INNER JOIN Degrees ON Degrees.DegreeId = StudentsECTS.DegreeId
     GROUP BY StudentRegistrationId, StudentId, Degrees.DegreeId, Degrees.TotalECTS, StudentsECTS.TotalECTS
     HAVING Degrees.TotalECTS <= StudentsECTS.TotalECTS
 );
