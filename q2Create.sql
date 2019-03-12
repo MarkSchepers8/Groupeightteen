50be6c2a7a479c8e2725e3148cff421c17cfb22e
HanSwinkels <hanswinkels@hotmail.com>
2019-03-12 05:51:52 +0100

    Left out query 2

--- q2Create.sql
+++ q2Create.sql
@@ -7,10 +7,6 @@ CREATE MATERIALIZED VIEW GPA as (
     GROUP BY StudentRegistrationsToDegrees.StudentRegistrationId
 );
 
-CREATE MATERIALIZED VIEW StudentsFailedCourse as (
-  SELECT StudentRegistrationId FROM CourseRegistrations WHERE grade < 5 group by studentregistrationid
-);
-
 CREATE MATERIALIZED VIEW CourseRegistrations2018_q1 as (
     SELECT CourseOffers.CourseOfferId, studentregistrationid, grade
     FROM CourseOffers
